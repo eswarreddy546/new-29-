1cce154046e6f962694cff24cd4f4fa1b2055179
Asri <asri>
2025-09-29 13:21:53 +0530

    reddis

---
+++ mysql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/new-29-"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+valid (){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+dnf install mysql-server -y
+valid $? " installing mysql server"
+
+systemctl enable mysqld
+valid $? "enable mysqld"
+
+systemctl start mysqld  
+valid $? "start mysqld"
+
+
+mysql_secure_installation --set-root-pass RoboShop@1
+valid $? "installing secure root"
